54fa3fe04d026d64aa8381d46955374ff31089ff
arnab <arnab.lalaofficial@gmail.com>
2020-07-06 13:05:31 +0530

    My new FIle in here

---
+++ testsCopy.py
@@ -0,0 +1,18 @@
+mydata=[]
+data=[1,2,3,4,5]
+for i in data:
+    if i in data:
+        mydata=mydata.append(i)
+
+for i in mydata:
+    print(mydata)
+
+
+mydata=[]
+data=[1,2,3,4,5]
+for i in data:
+    if i in data:
+        mydata=mydata.append(i)
+
+for i in mydata:
+    print(mydata
