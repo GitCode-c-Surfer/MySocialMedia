d158cdd1e250e128fd465291b457db84abd7a702
arnab <arnab.lalaofficial@gmail.com>
2020-07-06 13:04:26 +0530

    Mynew File

--- tests.py
+++ tests.py
@@ -6,3 +6,13 @@ for i in data:
 
 for i in mydata:
     print(mydata)
+
+
+mydata=[]
+data=[1,2,3,4,5]
+for i in data:
+    if i in data:
+        mydata=mydata.append(i)
+
+for i in mydata:
+    print(mydata
